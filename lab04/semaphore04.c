7907c0c8d72e3f0c7f7eb19262964097fc01b1f8
Carlos Salvatierra <cslucano@gmail.com>
2015-05-28 04:26:38 +0000

    sincronizacion primera clase

--- lab04/semaphore04.c
+++ lab04/semaphore04.c
@@ -3,7 +3,6 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-int saldo = 0;
 sem_t semA;
 sem_t semB;
 sem_t semC;
@@ -11,7 +10,9 @@ sem_t semC;
 void * imprimirA() {
     while(1){
         sem_wait(&semA);
+        sem_wait(&semA);
         printf("A\n");
+        sem_post(&semB);
         sem_post(&semB);
     }
 }
@@ -20,7 +21,7 @@ void * imprimirB() {
     while(1){
         sem_wait(&semB);
         printf("B\n");
-        sem_post(&semC);
+        sem_post(&semA);
     }
 }
 
@@ -40,12 +41,18 @@ void main()
     pthread_t thread_2;
     pthread_t thread_3;
 
-    sem_init(&semA, 0, 0);
+    sem_init(&semA, 0, 2);
     sem_init(&semB, 0, 0);
     sem_init(&semC, 0, 0);
 
     pthread_create(&thread_1, NULL, imprimirA, NULL);
+    pthread_create(&thread_1, NULL, imprimirA, NULL);
+    pthread_create(&thread_1, NULL, imprimirA, NULL);
     pthread_create(&thread_2, NULL, imprimirB, NULL);
+    pthread_create(&thread_2, NULL, imprimirB, NULL);
+    pthread_create(&thread_2, NULL, imprimirB, NULL);
+    pthread_create(&thread_3, NULL, imprimirC, NULL);
+    pthread_create(&thread_3, NULL, imprimirC, NULL);
     pthread_create(&thread_3, NULL, imprimirC, NULL);
 
     pthread_join(thread_1, NULL);
